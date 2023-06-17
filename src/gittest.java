d95f7186842637a671b6963082954d5d7e135323
riverLaugh <2314398305@qq.com>
2023-06-17 17:30:28 +0800

    update

--- src/gittest.java
+++ src/gittest.java
@@ -2,7 +2,7 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
-        System.out.println("bbbbbbbbbbbbbbbbbb");
+
 
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
